08bdce002f978a25dc88c666120b3b5114211f07
Brahim Elkartouch <belkarto@e2r7p6.1337.ma>
2022-12-08 02:08:17 +0100

    first push

---
+++ ft_memmove.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: belkarto <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/07 14:27:01 by belkarto          #+#    #+#             */
+/*   Updated: 2022/10/21 11:33:11 by belkarto         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "libft.h"
+
+void	*ft_memmove(void *dst, const void *src, size_t len)
+{
+	size_t	i;
+	char	*p_dst;
+	char	*p_src;
+
+	i = 0;
+	p_dst = dst;
+	p_src = (char *)src;
+	if (dst > src)
+	{
+		while (len > 0)
+		{
+			p_dst[len - 1] = p_src[len - 1];
+			len--;
+		}
+	}
+	else
+		ft_memcpy(dst, src, len);
+	return ((void *)dst);
+}
